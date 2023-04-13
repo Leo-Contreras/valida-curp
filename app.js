8f6f22e89616e9fc148477f94b45a73e4f805c99
Leo Contreras <leonardoaguie@gmail.com>
2023-04-13 14:27:45 -0700

    version 2

--- app.js
+++ app.js
@@ -6,12 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var curpRouter = require('./routes/curp'); // Añade esta línea
-
+var curpRouter = require('./routes/curp');
 
 var app = express();
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,10 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.use('/users', usersRouter);
-app.use('/curp', curpRouter); // Añade esta línea
-
+app.use('/curp', curpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
